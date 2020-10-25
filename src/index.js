391eec1514bb00bd3298dfc941d42f3160eac794
whattablackhole <rudolf.gritsenyak@gmail.com>
2020-10-25 22:18:44 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+  let massive = n.toString().split('');
+  let negative = false;
+  massive.forEach(element => {
+      if (element === '-'){
+          negative = true;
+          
+      }
+  });
+  if(negative){
+      return Math.sign(1)*parseInt(massive.reverse().join(''));
+  }
+  else{
+      return parseInt(massive.reverse().join(''));
+  }
 }
